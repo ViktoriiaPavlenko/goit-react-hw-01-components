46742c3eca4bf19bacd5cb77abbfca49e37afe02
ViktoriiaPavlenko <vktr9999@ukr.net>
2021-11-25 12:47:54 +0200

    first and second task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
+import Statistics from './components/Statistics/Statistics';
+import data from './components/Statistics/data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />;
+      {/* <Statistics stats={data} />; */}
     </div>
   );
 }
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import styles from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => {
+    return (
+        <section className={styles.statistics}>
+            <h2 className={styles.title}>{title}</h2>
+            <ul className={styles.list}>
+                {stats.map(item => (
+                    <li key={item.id}
+                    className={styles.item}>
+                    <span className={styles.label}>{item.label}</span>
+                    <span className={styles.percentage}>{item.percentage}%</span>
+                </li>
+                ))} 
+            </ul>
+        </section>
+    );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
+
+export default Statistics;
